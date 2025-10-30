e4c6b5fe458f0d725a25829fe7542e29a82bad8e
Ali Bouzar <abouzar@optimizerx.com>
2025-10-30 11:36:18 +0100

    add spring security models, dtos and user mapper

---
+++ src/main/java/com/miaoubich/security/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.miaoubich.security.mapper;
+
+import com.miaoubich.security.dto.AuthenticationResponse;
+import com.miaoubich.security.dto.RegisterRequest;
+import com.miaoubich.security.model.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User toEntity(RegisterRequest request);
+    AuthenticationResponse toResponse(String token);
+}
